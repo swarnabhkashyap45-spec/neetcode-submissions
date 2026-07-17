4f7b55ba9f3e858a18be265e1d90e1531c688818
swarnabhkashyap45-spec <swarnabhkashyap45@gmail.com>
2026-07-17 14:52:00 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        characterMap = {}
+        for c in s:
+            characterMap[c] = characterMap.get(c,0)+1
+        for c in t:
+            if c not in characterMap or characterMap[c]==0:
+                return False
+            characterMap[c] = characterMap.get(c)-1
+        for key in characterMap:
+            if characterMap[c]!=0:
+                return False
+        return True
